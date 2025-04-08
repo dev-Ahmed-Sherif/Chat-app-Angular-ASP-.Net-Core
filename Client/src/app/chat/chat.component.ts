93d90a17a1fa72b61c6ad192946099042a8e9716
Ahmed Sherif <ahmedsherif91101@gmail.com>
2025-04-08 06:52:07 +0200

    auth, login guards

--- Client/src/app/chat/chat.component.ts
+++ Client/src/app/chat/chat.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { ChatSidebarComponent } from '../components/chat-sidebar/chat-sidebar.component';
 
 @Component({
   selector: 'app-chat',
-  imports: [],
+  imports: [ChatSidebarComponent],
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.css'
+  styleUrl: './chat.component.css',
 })
-export class ChatComponent {
-
-}
+export class ChatComponent {}
